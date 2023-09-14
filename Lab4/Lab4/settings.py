f9e0bd51dbed48991ace84d5028a4856e25caba3
Victoryia Zviahintsava <vika.zv04@yandex.ru>
2023-09-14 11:10:30 +0300

    add media url & root

--- Lab4/Lab4/settings.py
+++ Lab4/Lab4/settings.py
@@ -130,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'fitness_club/static/fitness_club/media')
